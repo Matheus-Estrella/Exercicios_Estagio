b2001d090afa6c41d26946de8aab2f6a6b32947e
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-04-19 15:30:30 -0300

    Atualização_1

---
+++ Numero_Primo/Numero_Primo/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1-) Faça um programa que leia um número e informe se este número é primo;
+
+int primo, num;
+
+do
+{
+    Console.WriteLine("\nDigite um número para verificar se é ou não primo:");
+    num = int.Parse(Console.ReadLine());
+    if (num == 0)
+    {
+        Console.WriteLine("\nInsira um valor válido, números primos são positivos\n");
+    }
+} while (num >0);
+
